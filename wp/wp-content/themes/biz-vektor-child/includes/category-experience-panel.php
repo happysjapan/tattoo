5554e520fbed20373c82939bf4ff8e92c9af5675
三光楼茉澄 <sankoro@neopa.jp>
2016-04-19 17:44:55 +0900

    Adding new custom field to experience

--- wp/wp-content/themes/biz-vektor-child/includes/category-experience-panel.php
+++ wp/wp-content/themes/biz-vektor-child/includes/category-experience-panel.php
@@ -5,7 +5,7 @@
         <h3 class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
         <div class="profileWrap">
 
-          <div class="leftWrap">
+          <!-- <div class="leftWrap">
             <div class="categoryTermsWrap">
               <ul>
                 <?php $tax_terms = wp_get_post_terms( get_the_id(), 'experience-cat' );
@@ -14,28 +14,55 @@
                 <?php } ?>
               </ul>
             </div>
-          </div>
+          </div> -->
 
           <div class="detailWrap">
 
-            <?php while(have_rows('seminar_table')): the_row();
-            $date = strtotime(get_sub_field('seminar_opening_date'));
-            ?>
-            <table>
-              <tbody>
-                <tr>
-                  <th>日時</th>
-                  <td>
-                    <?php echo date('Y', $date).'年'.date('n', $date).'月'.date('d', $date).'日'; ?>
-                    <?php echo the_sub_field('seminar_opening_time'); ?>
-                  </td>
-                </tr>
-                <tr>
-                  <th>会場</th>
-                  <td><?php echo the_sub_field('seminar_place'); ?></td>
-                </tr>
-              </tbody>
-            </table>
+            <?php while(have_rows('experience_table')): the_row(); ?>
+            <ul class="experienceTable row">
+              <li class="medium-6 columns">
+                <dl>
+                  <dt>治療した傷跡</dt>
+                  <dd><?php echo the_sub_field('exp_treatment_type'); ?></dd>
+                </dl>
+              </li>
+              <li class="medium-6 columns">
+                <dl>
+                  <dt>治療方法</dt>
+                  <dd><?php echo the_sub_field('exp_treatment_method'); ?></dd>
+                </dl>
+              </li>
+              <li class="medium-6 columns">
+                <dl>
+                  <dt>治療期間</dt>
+                  <dd><?php echo the_sub_field('exp_treatment_period'); ?></dd>
+                </dl>
+              </li>
+              <li class="medium-6 columns">
+                <dl>
+                  <dt>氏名</dt>
+                  <dd><?php echo the_sub_field('exp_name'); ?></dd>
+                </dl>
+              </li>
+              <li class="medium-6 columns">
+                <dl>
+                  <dt>性別</dt>
+                  <dd><?php echo the_sub_field('exp_sex'); ?></dd>
+                </dl>
+              </li>
+              <li class="medium-6 columns">
+                <dl>
+                  <dt>住所</dt>
+                  <dd><?php echo the_sub_field('exp_address'); ?></dd>
+                </dl>
+              </li>
+              <li class="medium-6 columns">
+                <dl>
+                  <dt>利用した病院／クリニック</dt>
+                  <dd><?php echo the_sub_field('exp_clinic'); ?></dd>
+                </dl>
+              </li>
+            </ul>
             <?php endwhile; ?>
 
             <div class="textWrap">
@@ -44,6 +71,9 @@
           </div>
         </div>
       </div>
-
-      <p><a href="<?php the_permalink(); ?>"" class="linkToDetail" title="詳細ページへ">▶︎&nbsp;詳細ページへ</a></p>
+      <div class="btnWrap">
+        <a href="<?php the_permalink(); ?>" title="詳細を見る" class="btnLightBlue">詳細を見る</a>
+        <a href="tel:0120-697-182" title="無料で電話相談" class="btnLightGreen">無料で電話相談</a>
+        <a href="/contact-form/" title="フォームで相談" class="btnBlue">フォームで相談</a>
+      </div>
   </article><!-- .entry-content -->
