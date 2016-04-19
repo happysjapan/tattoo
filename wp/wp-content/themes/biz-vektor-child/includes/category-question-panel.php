326af525ddbb8cb6c0436e8da0f24d2d418065ab
julian-printemps <julian.printemps@gmail.com>
2016-04-19 15:42:40 +0900

    Fix images question cards

--- wp/wp-content/themes/biz-vektor-child/includes/category-question-panel.php
+++ wp/wp-content/themes/biz-vektor-child/includes/category-question-panel.php
@@ -1,7 +1,11 @@
 <article id="post-<?php the_ID(); ?>" class="article table--cell question-card">
   <div class="inner">
     <?php $image = get_field('question_image'); ?>
-    <img src="<?php echo $image['url']; ?>" class="question-card--image">
+    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+      <div class="question-card--image-holder">
+        <img src="<?php echo $image['url']; ?>" class="question-card--image">
+      </div>
+    </a>
     <h3 class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
     <div class="profileWrap">
       <div class="detailWrap">
